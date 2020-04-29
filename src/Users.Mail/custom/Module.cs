8e9fa34da024a7d387c2de043bf59cd8cce9a3a7
Peter Ombwa <peombwa@microsoft.com>
2020-04-29 16:35:22 -0700

    Add a custom module.cs

---
+++ src/Users.Mail/custom/Module.cs
@@ -0,0 +1,11 @@
+namespace Microsoft.Graph.PowerShell.Cmdlets.UsersMail
+{
+    using static Microsoft.Graph.PowerShell.Cmdlets.UsersMail.Runtime.Extensions;
+
+    /// <summary>A class that contains the module-common code and data.</summary>
+    public partial class Module
+    {
+        /// <summary>The default profile to load.</summary>
+        public string ProfileName = "latest-v1.0-beta";
+    }
+}
